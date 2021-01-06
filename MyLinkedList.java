1581e64b1f9e5bee48a56ac03a58fdf5f5107bbd
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2021-01-06 17:04:10 -0500

    Created MyLinkedList constructor and size method.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,5 +2,13 @@ public class MyLinkedList{
   private int size;
   private Node start,end;
 
+  public MyLinkedList(int Size){
+    size = Size;
+  }
 
+  public int size(){
+    return size;
+  }
+
+  
 }
