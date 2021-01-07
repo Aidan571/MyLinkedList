1f08d4a9af1ad4330ca562a8cba7ee3076213bff
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2021-01-07 18:20:27 -0500

    Created add to end of LinkedList method.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,13 +2,37 @@ public class MyLinkedList{
   private int size;
   private Node start,end;
 
-  public MyLinkedList(int Size){
-    size = Size;
+  public MyLinkedList(){
+    size = 0;
   }
 
   public int size(){
     return size;
   }
 
-  
+  public boolean add(String value){
+    Node x = new Node(value);
+    if(this.size() == 0){
+      start = x;
+      end = x;
+      x.setPrev(null);
+      x.setNext(null);
+      size += 1;
+      return true;
+    }
+    else if(this.size() == 1){
+      x.setPrev(end);
+      end = x;
+      start.setNext(x);
+      x.setNext(null);
+      size += 1;
+      return true;
+    }
+    else
+    x.setPrev(end);
+    end = x;
+    x.setNext(null);
+    size += 1;
+    return true;
+  }
 }
