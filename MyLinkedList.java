5ecca9e6c16f0e724d4d62e35aaa180947898c47
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2021-01-07 19:08:17 -0500

    Created add at index method.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -35,4 +35,87 @@ public class MyLinkedList{
     size += 1;
     return true;
   }
+
+  private Node whatsNext(int index){
+    Node x = start;
+    Node y;
+    for(int i = 0; i < index; i++){
+      y = x.getNext();
+      x = y;
+    }
+    return x.getNext();
+  }
+
+  private Node whatsPrev(int index){
+    Node x = start;
+    Node y;
+    for(int i = 0; i < index - 1; i++){
+      y = x.getNext();
+      x = y;
+    }
+    return x.getNext();
+  }
+
+  public boolean add(int index, String value){
+    Node x = new Node(value);
+    Node holder0;
+    Node holder1;
+    if(index < 0){
+      throw new IndexOutOfBoundsException("Index can not be negative");
+    }
+    else if(index > this.size()){
+      throw new IndexOutOfBoundsException("Index: " + index + " can not be larger than the size of the LinkedList");
+    }
+    else if(this.size() == 0){
+      start = x;
+      end = x;
+      x.setPrev(null);
+      x.setNext(null);
+      size += 1;
+      return true;
+    }
+    else if(this.size() == 1){
+      if(index == 0){
+        start = x;
+        end.setPrev(x);
+        x.setPrev(null);
+        x.setNext(end);
+        size += 1;
+        return true;
+      }
+      else
+      end = x;
+      start.setNext(x);
+      x.setPrev(start);
+      x.setNext(null);
+      size += 1;
+      return true;
+    }
+    else
+    if(index == 0){
+      start.setPrev(x);
+      x.setNext(start);
+      start = x;
+      x.setPrev(null);
+      size += 1;
+      return true;
+    }
+    else if(index == this.size()){
+      end.setNext(x);
+      x.setPrev(end);
+      end = x;
+      x.setNext(null);
+      size += 1;
+      return true;
+    }
+    else
+    holder0 = this.whatsNext(index);
+    holder1 = this.whatsPrev(index);
+    this.whatsNext(index).setPrev(x);
+    this.whatsPrev(index).setNext(x);
+    x.setNext(holder0);
+    x.setPrev(holder1);
+    size += 1;
+    return true;
+  }
 }
